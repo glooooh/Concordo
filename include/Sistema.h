a70f70dec5c925de258ad0e6c6d0602cc1b17ec2
glooooh <glorinhamsouza@gmail.com>
2023-06-30 18:25:47 -0300

    Novas funções da parte 2 do projeto

--- include/Sistema.h
+++ include/Sistema.h
@@ -5,8 +5,12 @@
 #ifndef SISTEMA_H
 #define SISTEMA_H
 
-#include "Usuario.h"
-#include "Servidor.h"
+#include "Usuario.h"    /**< Inclui a classe Usuario. */
+#include "Mensagem.h"   /**< Inclui a classe Mensagem. */
+#include "Servidor.h"   /**< Inclui a classe Servidor. */
+#include "Canal.h"      /**< Inclui a classe Canal. */
+#include "CanalTexto.h" /**< Inclui a classe CanalTexto. */
+#include "CanalVoz.h"   /**< Inclui a classe CanalVoz. */
 
 /**
  * @class Sistema
@@ -35,6 +39,10 @@ public:
     /**
      * @brief Adiciona um novo usuário ao sistema.
      *
+     * @param email O email do usuário.
+     * @param senha A senha do usuário.
+     * @param nome O nome do usuário.
+     *
      * @return Retorna True se o cadastro foi bem sucedido, False caso contrário.
      */
     bool adicionarUsuario(string, string, string);
@@ -42,12 +50,17 @@ public:
     /**
      * @brief Busca um usuário pelo índice.
      *
+     * @param indiceBuscado O índice do usuário a ser buscado.
+     *
      * @return Um ponteiro para o usuário encontrado, ou nullptr se o índice for inválido.
      */
     Usuario *buscarPeloID(int);
 
     /**
      * @brief Realiza o login do usuário no sistema.
+     *
+     * @param email O email do usuário.
+     * @param senha A senha do usuário.
      *
      * @return True se o login for bem-sucedido, False caso contrário.
      */
@@ -63,6 +76,9 @@ public:
     /**
      * @brief Busca um servidor pelo nome.
      *
+     * @param nome O nome do servidor a ser buscado.
+     * @param msgDeErro A mensagem de erro a ser exibida caso o servidor não seja encontrado.
+     *
      * @return Um ponteiro para o servidor encontrado, ou nullptr caso não seja encontrado.
      */
     Servidor *buscarServidorPorNome(string, string);
@@ -70,17 +86,25 @@ public:
     /**
      * @brief Busca um servidor pelo nome e retorna o índice encontrado.
      *
+     * @param nome O nome do servidor a ser buscado.
+     * @param index A referência para armazenar o índice do servidor encontrado.
+     * @param msgDeErro A mensagem de erro a ser exibida caso o servidor não seja encontrado.
+     *
      * @return Um ponteiro para o servidor encontrado, ou nullptr caso não seja encontrado.
      */
     Servidor *buscarServidorPorNome(string, int &, string);
 
     /**
      * @brief Cria um novo servidor no sistema.
+     *
+     * @param nome O nome do servidor a ser criado.
      */
     void criarServidor(string);
 
     /**
      * @brief Remove um servidor do sistema.
+     *
+     * @param nome O nome do servidor a ser removido.
      *
      * @return Retorna true se a remoção foi bem-sucedida, e false caso contrário.
      */
@@ -94,22 +118,34 @@ public:
     /**
      * @brief Verifica se o usuário logado é o dono do servidor.
      *
+     * @param server O servidor a ser verificado.
+     * @param mensagemDeErro A mensagem de erro a ser exibida caso o usuário não seja o dono do servidor.
+     *
      * @return Retorna true se o usuário logado for o dono do servidor, e false caso contrário.
      */
     bool verificarDonoServidor(Servidor *, string);
 
     /**
      * @brief Modifica a descrição de um servidor.
+     *
+     * @param nome O nome do servidor.
+     * @param descricao A nova descrição a ser atribuída.
      */
     void modificarDescricaoDeServidor(string, string);
 
     /**
      * @brief Modifica o código de convite de um servidor.
+     *
+     * @param nome O nome do servidor.
+     * @param codigo O novo código de convite a ser atribuído.
      */
     void modificarCodigoServidor(string, string);
 
     /**
      * @brief Entra em um servidor com o nome e código de convite fornecidos.
+     *
+     * @param nome O nome do servidor a entrar.
+     * @param cod O código de convite do servidor.
      */
     void entrarEmServidor(string, string);
 
@@ -120,8 +156,58 @@ public:
 
     /**
      * @brief Lista os participantes do servidor atual.
+     *
+     * @details Este método obtém a lista de IDs dos participantes do servidor atual. Em seguida,
+     * imprime os nomes correspondentes.
      */
     void listarParticipantesDoServidor();
+
+    /**
+     * @brief Busca um canal no servidor atual pelo nome.
+     *
+     * @param nome O nome do canal a ser buscado.
+     *
+     * @return Um ponteiro para o canal encontrado, ou nullptr caso não seja encontrado.
+     */
+    Canal *buscarCanalPorNome(string);
+
+    /**
+     * @brief Lista os canais do servidor atual.
+     */
+    void listarCanais();
+
+    /**
+     * @brief Cria um novo canal no servidor atual.
+     *
+     * @param nome O nome do canal a ser criado.
+     * @param tipo O tipo do canal a ser criado.
+     */
+    void criarCanal(string, string);
+
+    /**
+     * @brief Entra em um canal no servidor atual.
+     *
+     * @param nome O nome do canal a ser visualizado.
+     */
+    void entrarEmCanal(string);
+
+    /**
+     * @brief Sai do canal que está sendo visualizado no momento.
+     */
+    void sairDoCanal();
+
+    /**
+     * @brief Envia uma mensagem no canal atualmente visualizado.
+     *
+     * @param texto O conteúdo da mensagem a ser enviada.
+     * @param dataHora A data e hora em que a mensagem está sendo enviada.
+     */
+    void enviarMensagem(string, string);
+
+    /**
+     * @brief Lista as mensagens do canal atualmente visualizado.
+     */
+    void listarMensagens();
 };
 
 #endif
--- src/Sistema.cpp
+++ src/Sistema.cpp
@@ -77,7 +77,7 @@ bool Sistema::adicionarUsuario(string email, string senha, string nome)
  * @brief Busca um usuário pelo índice.
  *
  * @param indiceBuscado O índice do usuário a ser buscado.
- * 
+ *
  * @return Um ponteiro para o usuário encontrado, ou nullptr se o índice for inválido.
  */
 Usuario *Sistema::buscarPeloID(int indiceBuscado)
@@ -99,7 +99,7 @@ Usuario *Sistema::buscarPeloID(int indiceBuscado)
  *
  * @param email O email do usuário.
  * @param senha A senha do usuário.
- * 
+ *
  * @return True se o login for bem-sucedido, False caso contrário.
  */
 bool Sistema::login(string email, string senha)
@@ -152,23 +152,26 @@ bool Sistema::disconnect()
  * @brief Busca um servidor pelo nome.
  *
  * @param nome O nome do servidor a ser buscado.
+ * @param msgDeErro A mensagem de erro a ser exibida caso o servidor não seja encontrado.
+ *
  * @return Um ponteiro para o servidor encontrado, ou nullptr caso não seja encontrado.
  */
 Servidor *Sistema::buscarServidorPorNome(string nome, string msgDeErro)
 {
-    for (int ii = 0; ii < servidoresDoSistema->size(); ii++)
+    /* Percorre os servidores cadastrados no sistema. */
+    for (Servidor *servidor : *servidoresDoSistema)
     {
-        // Verifica se o email e senha fornecidos correspondem a algum usuário digitado.
-        if ((*servidoresDoSistema)[ii]->getNome() == nome)
-        {
-            return (*servidoresDoSistema)[ii];
-        }
+        /* Verifica se o nome fornecido corresponde ao nome de algum servidor cadastrado. */
+        if (servidor->getNome() == nome)
+            return servidor;
     }
 
+    /* Imprime mensagem de erro, se houver uma e for necessária. */
     if (msgDeErro != "")
     {
         cout << msgDeErro << endl;
     }
+    /* Retornar nullptr caso o servidor não exista. */
     return nullptr;
 }
 
@@ -177,24 +180,28 @@ Servidor *Sistema::buscarServidorPorNome(string nome, string msgDeErro)
  *
  * @param nome O nome do servidor a ser buscado.
  * @param index A referência para armazenar o índice do servidor encontrado.
+ * @param msgDeErro A mensagem de erro a ser exibida caso o servidor não seja encontrado.
+ *
  * @return Um ponteiro para o servidor encontrado, ou nullptr caso não seja encontrado.
  */
 Servidor *Sistema::buscarServidorPorNome(string nome, int &index, string msgDeErro)
 {
     for (int ii = 0; ii < servidoresDoSistema->size(); ii++)
     {
-        // Verifica se o email e senha fornecidos correspondem a algum usuário digitado.
+        /* Verifica se o nome fornecido corresponde ao nome de algum servidor cadastrado. */
         if ((*servidoresDoSistema)[ii]->getNome() == nome)
         {
-            index = ii;
+            index = ii; // Armazena o índice do servidor encontrado na variável index.
             return (*servidoresDoSistema)[ii];
         }
     }
 
+    /* Imprime mensagem de erro, se houver uma e for necessária. */
     if (msgDeErro != "")
     {
         cout << msgDeErro << endl;
     }
+    /* Retornar nullptr caso o servidor não exista. */
     return nullptr;
 }
 
@@ -232,6 +239,7 @@ void Sistema::criarServidor(string nome)
  * @brief Remove um servidor do sistema.
  *
  * @param nome O nome do servidor a ser removido.
+ *
  * @return Retorna true se a remoção foi bem-sucedida, e false caso contrário.
  */
 bool Sistema::removerServidor(string nome)
@@ -243,6 +251,7 @@ bool Sistema::removerServidor(string nome)
         return false;
     }
 
+    /* Verifica se o servidor existe. */
     int index;
     string msgDeErro = "Servidor '" + nome + "' nao encontrado";
     Servidor *server = buscarServidorPorNome(nome, index, msgDeErro);
@@ -252,7 +261,7 @@ bool Sistema::removerServidor(string nome)
         return false;
     }
 
-    // Verificar se o servidor escolhido existe e se o dono do servidor é o usuário logado.
+    // Verificar se o dono do servidor é o usuário logado.
     msgDeErro = "Voce nao e o dono do servidor '" + server->getNome() + "'";
     if (server == nullptr || !verificarDonoServidor(server, msgDeErro))
     {
@@ -302,6 +311,7 @@ void Sistema::listarServidores()
  *
  * @param server O servidor a ser verificado.
  * @param mensagemDeErro A mensagem de erro a ser exibida caso o usuário não seja o dono do servidor.
+ *
  * @return Retorna true se o usuário logado for o dono do servidor, e false caso contrário.
  */
 bool Sistema::verificarDonoServidor(Servidor *server, string mensagemDeErro)
@@ -417,9 +427,9 @@ void Sistema::entrarEmServidor(string nome, string cod)
     }
 
     /* Verifica se o usuário já está na lista de participantes do servidor. */
-    for (size_t ii = 0; ii < server->getParticipantesID().size(); ii++)
+    for (int participanteID : server->getParticipantesID())
     {
-        if (server->getParticipantesID()[ii] == usuarioLogadoAtual->getID())
+        if (participanteID == usuarioLogadoAtual->getID())
         {
             servidorAtual = server;
             cout << "Entrou no servidor com sucesso" << endl;
@@ -503,5 +513,178 @@ void Sistema::listarParticipantesDoServidor()
     for (size_t ii = 0; ii < listaParticipantes.size(); ii++)
     {
         cout << buscarPeloID(listaParticipantes[ii])->getNome() << endl;
+    }
+}
+
+/**
+ * @brief Busca um canal no servidor atual pelo nome.
+ *
+ * @param nome O nome do canal a ser buscado.
+ * 
+ * @return Um ponteiro para o canal encontrado, ou nullptr caso não seja encontrado.
+ */
+Canal *Sistema::buscarCanalPorNome(string nome)
+{
+    for (Canal *canal : servidorAtual->getCanais())
+    {
+        if (canal->getNome() == nome)
+        {
+            return canal;
+        }
+    }
+
+    return nullptr;
+}
+
+/**
+ * @brief Lista os canais do servidor atual.
+ */
+void Sistema::listarCanais()
+{
+    // Verifica se o usuário está em algum servidor.
+    if (servidorAtual == nullptr)
+    {
+        cout << "Voce nao esta visualizando nenhum servidor" << endl;
+        return;
+    }
+
+    if (servidorAtual->getCanais().size() == 0)
+    {
+        cout << "Nenhum canal cadastrado" << endl;
+        return;
+    }
+
+    vector<Canal *> canais = servidorAtual->getCanais();
+
+    cout << "#canais de texto" << endl;
+    for (Canal *canal : canais)
+    {
+        string tipo = canal->getTipo();
+        if (tipo == "CanalTexto")
+        {
+            cout << canal->getNome() << endl;
+        }
+    }
+
+    cout << "#canais de audio" << endl;
+    for (Canal *canal : canais)
+    {
+        string tipo = canal->getTipo();
+        if (tipo == "CanalVoz")
+        {
+            cout << canal->getNome() << endl;
+        }
+    }
+}
+
+/**
+ * @brief Cria um novo canal no servidor atual.
+ *
+ * @param nome O nome do canal a ser criado.
+ * @param tipo O tipo do canal a ser criado.
+ */
+void Sistema::criarCanal(string nome, string tipo)
+{
+    // Verifica se o usuário está em algum servidor.
+    if (servidorAtual == nullptr)
+    {
+        cout << "Voce nao esta visualizando nenhum servidor" << endl;
+        return;
+    }
+
+    servidorAtual->criarCanal(nome, tipo);
+}
+
+/**
+ * @brief Entra em um canal no servidor atual.
+ *
+ * @param nome O nome do canal a ser visualizado.
+ */
+void Sistema::entrarEmCanal(string nome)
+{
+    // Verifica se o usuário está em algum servidor.
+    if (servidorAtual == nullptr)
+    {
+        cout << "Voce nao esta visualizando nenhum servidor" << endl;
+        return;
+    }
+
+    Canal *canal = buscarCanalPorNome(nome);
+
+    if (!canal)
+    {
+        cout << "Canal '" << nome << "' nao existe" << endl;
+        return;
+    }
+
+    cout << "Entrou no canal '" << nome << "'" << endl;
+    canalAtual = canal;
+}
+
+/**
+ * @brief Sai do canal que está sendo visualizado no momento.
+ */
+void Sistema::sairDoCanal()
+{
+    // Verifica se o usuário está em algum canal.
+    if (canalAtual == nullptr)
+    {
+        cout << "Voce nao esta visualizando nenhum canal" << endl;
+        return;
+    }
+
+    cout << "Saindo do canal" << endl;
+    canalAtual = nullptr;
+}
+
+/**
+ * @brief Envia uma mensagem no canal atualmente visualizado.
+ *
+ * @param texto O conteúdo da mensagem a ser enviada.
+ * @param dataHora A data e hora em que a mensagem está sendo enviada.
+ */
+void Sistema::enviarMensagem(string texto, string dataHora)
+{
+    // Verifica se o usuário está em algum canal.
+    if (canalAtual == nullptr)
+    {
+        cout << "Voce nao esta visualizando nenhum canal" << endl;
+        return;
+    }
+
+    Mensagem mensagem(texto, usuarioLogadoAtual->getID(), dataHora);
+
+    canalAtual->enviarMensagem(mensagem);
+
+    cout << "Mensagem enviada com sucesso" << endl;
+}
+
+/**
+ * @brief Lista as mensagens do canal atualmente visualizado.
+ */
+void Sistema::listarMensagens()
+{
+    // Verifica se o usuário está em algum canal.
+    if (canalAtual == nullptr)
+    {
+        cout << "Voce nao esta visualizando nenhum canal" << endl;
+        return;
+    }
+
+    /* Verificar se existem mensagens no canal. */
+    if (canalAtual->getMensagens().size() == 0)
+    {
+        cout << "Sem mensagens para exibir" << endl;
+        return;
+    }
+
+    for (Mensagem mensagem : canalAtual->getMensagens())
+    {
+        /* Imprime as mensagens armazenadas no canal. */
+        string nomeUsuario, dataHora, conteudo;
+        nomeUsuario = buscarPeloID(mensagem.getIdRemetente())->getNome();
+        dataHora = mensagem.getDataHora();
+        conteudo = mensagem.getConteudo();
+        cout << nomeUsuario << "<" << dataHora << ">: " << conteudo << endl;
     }
 }
