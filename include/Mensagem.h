2c20e5a3ee07362bd86f4029a4f023a548b74137
glooooh <glorinhamsouza@gmail.com>
2023-06-30 18:23:38 -0300

    UPDATE classe 'MENSAGEM' - constr. & métodos get

--- include/Mensagem.h
+++ include/Mensagem.h
@@ -17,28 +17,57 @@ using namespace std;
 class Mensagem
 {
 private:
-    string dataHora;
-    int enviadaPor; /*<! Guardar o ID do usuário que enviou a mensagem */
-    string conteudo;
+    string dataHora; /**< A data e hora em que a mensagem foi enviada. */
+    int enviadaPor;  /*<! Guardar o ID do usuário que enviou a mensagem */
+    string conteudo; /**< O conteúdo da mensagem. */
 
 public:
     /**
      * @brief Construtor da classe Mensagem.
-     * 
-     * @param texto O texto da mensagem.
-     * @param idRemetente O id do usuário remetente da mensagem.
+     *
+     * @param texto O conteúdo da mensagem.
+     * @param idRemetente O ID do remetente da mensagem.
+     * @param data_hora A data e hora da mensagem.
      */
-    Mensagem(string texto, int idRemetente);
-    
+    Mensagem(const string &texto, int idRemetente, const string &);
+
     /**
      * @brief Construtor padrão da classe Mensagem.
-    */
+     */
     Mensagem();
-    
+
     /**
      * @brief Destrutor da classe Mensagem.
-    */
+     */
     ~Mensagem();
+
+    /**
+     * @brief Obtém a data e hora da mensagem.
+     *
+     * @return A data e hora da mensagem.
+     */
+    string getDataHora();
+
+    /**
+     * @brief Obtém o ID do remetente da mensagem.
+     *
+     * @return O ID do remetente da mensagem.
+     */
+    int getIdRemetente();
+
+    /**
+     * @brief Obtém o conteúdo da mensagem.
+     *
+     * @return O conteúdo da mensagem.
+     */
+    string getConteudo() const;
+
+    /**
+     * @brief Verifica se a mensagem está vazia.
+     *
+     * @return True se a mensagem estiver vazia, False caso contrário.
+     */
+    bool empty();
 };
 
 #endif
--- src/Mensagem.cpp
+++ src/Mensagem.cpp
@@ -7,9 +7,68 @@
 
 #include "Mensagem.h" /**< Inclui a classe Mensagem. */
 
-// Construtores
-Mensagem::Mensagem(string data_hora, int id, string msg) {}
+/**
+ * @brief Construtor da classe Mensagem.
+ * 
+ * @param texto O conteúdo da mensagem.
+ * @param idRemetente O ID do remetente da mensagem.
+ * @param data_hora A data e hora da mensagem.
+ */
+Mensagem::Mensagem(const string &texto, int idRemetente, const string &data_hora)
+{
+    dataHora = data_hora;
+    enviadaPor = idRemetente;
+    conteudo = texto;
+}
+
+/**
+ * @brief Construtor padrão da classe Mensagem.
+ */
 Mensagem::Mensagem() {}
 
-// Destrutor
+/**
+ * @brief Destrutor padrão da classe Mensagem.
+ */
 Mensagem::~Mensagem() {}
+
+/**
+ * @brief Obtém a data e hora da mensagem.
+ * 
+ * @return A data e hora da mensagem.
+ */
+string Mensagem::getDataHora()
+{
+    return dataHora;
+}
+
+/**
+ * @brief Obtém o ID do remetente da mensagem.
+ * 
+ * @return O ID do remetente da mensagem.
+ */
+int Mensagem::getIdRemetente()
+{
+    return enviadaPor;
+}
+
+/**
+ * @brief Obtém o conteúdo da mensagem.
+ * 
+ * @return O conteúdo da mensagem.
+ */
+string Mensagem::getConteudo() const
+{
+    return conteudo;
+}
+
+/**
+ * @brief Verifica se a mensagem está vazia.
+ * 
+ * @return True se a mensagem estiver vazia, False caso contrário.
+ */
+bool Mensagem::empty()
+{
+    if (dataHora.empty())
+        return true;
+    return false;
+}
